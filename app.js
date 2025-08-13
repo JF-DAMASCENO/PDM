cb3dac291a5972795915d34b23b930c378d0ce0a
JF-DAMASCENO <joaofelipydb@gmail.com>
2025-08-13 08:58:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
+    navigator.serviceWorker.register("/PDM 1308/sw.js").then((registration) => {
         console.log("Service Worker registrado com sucesso:", registration);
       })
       .catch((error) => {
@@ -10,3 +8,4 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
+
